7ad5d71c1290a4093e1bc159464717e394164fb6
Vidhya0501 <vidhyasuri97@gmail.com>
2023-09-04 14:49:21 +0530

    asyn-await updated

--- api_fetch.js
+++ api_fetch.js
@@ -2,29 +2,38 @@ const head=document.createElement("header");
 head.innerText="Users List";
 document.body.append(head);
 const sec=document.createElement("section");
-const result=fetch("https://jsonplaceholder.typicode.com/users");
-result
-  .then((data) => data.json())
-  .then((data1) => {
-    for (var i = 0; i < data1.length; i++) {
-      const div = document.createElement("div");
-      div.innerHTML = `<div class="row">
-      <div class="col-sm-6 mb-3 mb-sm-0">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Name : ${data1[i].name}</h5>
-                <p class="card-text">Email : ${data1[i].email}</P>
-                <p class="card-text">Street : ${data1[i].address.street}</P>
-                <p class="card-text">City : ${data1[i].address.city}</P>
+
+async function app(){
+  try{
+    const result=fetch("https://jsonplaceholder.typicode.com/users");
+    result
+      .then((data) => data.json())
+      .then((data1) => {
+        for (var i = 0; i < data1.length; i++) {
+          const div = document.createElement("div");
+          div.innerHTML = `<div class="row">
+          <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Name : ${data1[i].name}</h5>
+                    <p class="card-text">Email : ${data1[i].email}</P>
+                    <p class="card-text">Street : ${data1[i].address.street}</P>
+                    <p class="card-text">City : ${data1[i].address.city}</P>
+                </div>
             </div>
-        </div>
-      </div>
-      </div>`;
-    sec.append(div)
-    document.body.append(sec);
-    }
-});
+          </div>
+          </div>`;
+        sec.append(div)
+        document.body.append(sec);
+        }
+    });
 
+  }
+  catch(error){
+    console.log(error);
+  }
+}
 
+app();
 
 
